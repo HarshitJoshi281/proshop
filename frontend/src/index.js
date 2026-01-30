0f23209da6ecec18f912da76c12ced1a7bea6a46
HarshitJoshi281 <28harshit98@gmail.com>
2026-01-30 22:59:50 +0530

    Frontend ready for deployment with Render backend URL

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,10 @@ import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import axios from 'axios' // << add this line
+
+// << Add this line to set global base URL for all API calls >>
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
