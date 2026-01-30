c84f24a57e802bd1b24b9c7c7682113a872473da
HarshitJoshi281 <28harshit98@gmail.com>
2026-01-30 22:04:38 +0530

    Fix backend server config and start script

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
@@ -35,17 +34,9 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
-}
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
 
 app.use(notFound)
 app.use(errorHandler)
